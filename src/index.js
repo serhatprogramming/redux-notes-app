67e3e6d5f99d4b5c320451009a7dacae24ba9e34
serhatprogramming <serhatprogramming@gmail.com>
2023-04-13 15:31:21 -0400

    redux state with provider

--- src/index.js
+++ src/index.js
@@ -1,75 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import App from "./App";
 import noteReducer from "./reducers/noteReducer";
 
 const store = createStore(noteReducer);
 
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
-
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "the app state is in redux store",
-    important: true,
-    id: 1,
-  },
-});
-
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2,
-  },
-});
-
-const App = () => {
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value = "";
-    store.dispatch({
-      type: "NEW_NOTE",
-      payload: {
-        content,
-        important: false,
-        id: generateId(),
-      },
-    });
-  };
-
-  const toggleImportance = (id) => {
-    store.dispatch({
-      type: "TOGGLE_IMPORTANCE",
-      payload: { id },
-    });
-  };
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map((note) => (
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const renderApp = () => {
-  root.render(<App />);
-};
-
-renderApp();
-store.subscribe(renderApp);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
