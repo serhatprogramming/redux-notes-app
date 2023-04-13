d83cf06e7ec12dde554b8a590a16cb4395aba1a2
serhatprogramming <serhatprogramming@gmail.com>
2023-04-13 19:00:53 -0400

    combined reducers - filtering added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NewNote from "./component/NewNote";
 import Notes from "./component/Notes";
+import VisibilityFilter from "./component/VisibilityFilter";
 
 const App = () => {
   return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   );
